c666a5d3ca5c4fb5cd4b510bfe0369757af9bbed
Brandon Clapp <clapp09@gmail.com>
2017-08-15 08:16:07 -0500

    postJson function

--- src/packt-free-scraper/main.go
+++ src/packt-free-scraper/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"net/http"
 	"strings"
     "fmt"
     "log"
+    "io/ioutil"
     goquery "github.com/PuerkitoBio/goquery"
 )
 
@@ -21,9 +24,33 @@ func main() {
 
 func getBookTitle(doc *goquery.Document) string {
 
-    // todo: error checking?
-
     text := doc.Find("#deal-of-the-day > div > div > div.dotd-main-book-summary.float-left > div.dotd-title > h2").Text()
     text = strings.TrimSpace(text)
     return text
+}
+
+
+func postJson() {
+    url := "http://httpbin.org/post"
+    fmt.Println("URL:>", url)
+
+    myStr := "The Book Name Is Good"
+    _ = myStr
+
+    var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+    req, err := http.NewRequest("POSt", url, bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        // log vs panic?
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
 }
